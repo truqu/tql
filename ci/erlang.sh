1f76395452123eb8ae66991f040034993e4254b8
ilias <mail@ilias.xyz>
2018-02-07 15:02:56 +0100

    Switch CI to travis

--- ci/erlang.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-KERL_VERSION=20.2
-
-if [ ! -e $HOME/erlang/$KERL_VERSION ]; then
-    kerl update releases
-    kerl build $KERL_VERSION $KERL_VERSION
-    kerl install $KERL_VERSION ~/erlang/$KERL_VERSION
-fi
