7f419878d14e5d1268bd5a177ce6e3c7ba1b53b1
Ilias Van Peer <zwilias@users.noreply.github.com>
2018-03-02 11:14:30 +0100

    Switch to Travis and upgrade tested Erlang release (#2)
    * Bump erlang version to 20.2
    
    * Switch CI to travis
    
    * Run rebar3 in debug mode on travis
    
    Seems like it doesn't like our git:// dependencies, for some reason
    
    * Use `https` scheme rather than `git`

--- ci/kerl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -e $HOME/bin/kerl ]; then
-   cd $HOME/bin
-   wget https://raw.githubusercontent.com/kerl/kerl/master/kerl
-   chmod a+x kerl;
-fi
--- ci/rebar3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -e $HOME/bin/rebar3 ]; then
-   cd $HOME/bin
-   wget https://s3.amazonaws.com/rebar3/rebar3
-   chmod a+x rebar3;
-fi
